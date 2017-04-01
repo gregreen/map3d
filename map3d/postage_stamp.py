574af24a1108264de8ad331a732c3c9a0c9713d9
Gregory M. Green <gregorymgreen@gmail.com>
2017-04-01 14:19:15 -0700

    Postage stamps saved as JPEG. Smaller filesizes -> faster encoding, less data sent over network.

--- map3d/postage_stamp.py
+++ map3d/postage_stamp.py
@@ -28,36 +28,46 @@ rasterizer = proj_fast.MapRasterizerFast(mapdata.map_nside, img_shape, fov=2*rad
 import time
 
 def encode_image(img_arr, c_mask=(202,222,219)):
+    # Generate 8-bit grayscale image
     img_arr = img_arr.T[::-1,:]
     nan_mask = ~np.isfinite(img_arr)
-    
+
     img_arr[img_arr > 1.] = 1.
-    
+
     img_arr = (254. * img_arr).astype(np.uint8)
-    img_arr[nan_mask] = 255
-    
-    img = Image.fromarray(img_arr, mode='L')
-    
+    # img_arr[nan_mask] = 255
+
+    # Convert to color image with mask color
+    if np.any(nan_mask):
+        img_arr_color = np.empty(img_arr.shape+(3,), dtype=np.uint8)
+        for k in range(3):
+            img_arr_color[:,:,k] = img_arr[:,:]
+            img_arr_color[nan_mask,k] = c_mask[k]
+        img = Image.fromarray(img_arr_color, mode='RGB')
+    else:
+        img = Image.fromarray(img_arr, mode='L')
+
     t4 = time.time()
-    
+
     img_io = StringIO()
-    img.save(img_io, 'PNG', transparency=255, compress_level=2)
+    # img.save(img_io, 'PNG', transparency=255, compress_level=2)
+    img.save(img_io, 'JPEG', quality=40)
     img_io.seek(0)
-    
+
     t5 = time.time()
-    
+
     data = img_io.getvalue().encode('base64')
-    data = 'data:image/png;base64,{0}'.format(urllib.quote(data.rstrip('\n')))
-    
+    data = 'data:image/jpg;base64,{0}'.format(urllib.quote(data.rstrip('\n')))
+
     t6 = time.time()
-    
+
     print ''
     print 'save: %.4f s' % (t5-t4)
     print 'encode: %.4f s' % (t6-t5)
     print 'total: %.4f s' % (t6-t4)
     print 'size: %.2f kB' % (len(data) / 1024.)
     print ''
-    
+
     return data
 
 
@@ -73,28 +83,28 @@ def postage_stamps(l, b,
     #t2 = time.time()
     img = rasterizer.rasterize(pix_val, l, b)
     #t3 = time.time()
-    
+
     if difference:
         img[2] -= img[1]
         img[1] -= img[0]
-        
+
         idx = np.isfinite(img)
-        
+
         if np.sum(idx) == 0:
             return img
-        
+
         vmax = np.percentile(img[idx], 99.5)
     else:
         idx = np.isfinite(img[-1])
-        
+
         if np.sum(idx) == 0:
             return img
-        
+
         vmax = np.percentile(img[-1][idx], 99.)
-    
+
     for i in img:
         i *= 1./vmax
-    
+
     #t4 = time.time()
     #print '%.4f  %.4f  %.4f  %.4f' % (t1-t0, t2-t1, t3-t2, t4-t3)
     return img
@@ -104,7 +114,7 @@ def encode_imgfile(fname):
     f = open(os.path.join(media_path, fname), 'r')
     data = f.read().encode('base64')
     f.close()
-    
+
     data = 'data:image/png;base64,{0}'.format(urllib.quote(data.rstrip('\n')))
-    
+
     return data
